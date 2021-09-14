581559f4051926fe35ca63b66b9e348cf12844f5
jinsoo <admin@binary01.me>
2021-09-14 22:20:34 +0900

    feat: layout structure add

---
+++ src/pages/layout/header/index.jsx
@@ -0,0 +1,7 @@
+import * as S from '@pages/layout/header/style';
+
+const LayoutPageHeader = () => {
+  return <S.LayoutPageHeader>asd</S.LayoutPageHeader>;
+};
+
+export default LayoutPageHeader;
---
+++ src/pages/layout/index.jsx
@@ -0,0 +1,12 @@
+import LayoutPageHeader from '@pages/layout/header';
+import * as S from '@pages/layout/style';
+
+const LayoutPage = () => {
+  return (
+    <S.LayoutPageContainer>
+      <LayoutPageHeader>asd</LayoutPageHeader>
+    </S.LayoutPageContainer>
+  );
+};
+
+export default LayoutPage;
