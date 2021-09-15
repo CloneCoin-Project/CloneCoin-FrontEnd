4851d1632d9ed3cb959c4c5faec27a94aabcc71a
jinsoo <admin@binary01.me>
2021-09-15 12:07:59 +0900

    feat: layout header, content setting

---
+++ src/pages/layout/content/index.jsx
@@ -0,0 +1,7 @@
+import * as S from '@pages/layout/content/style';
+
+const LayoutPageContent = () => {
+  return <S.LayoutPagContent>Content</S.LayoutPagContent>;
+};
+
+export default LayoutPageContent;
--- src/pages/layout/style.js
+++ src/pages/layout/style.js
@@ -2,5 +2,5 @@ import styled from 'styled-components';
 import { Layout } from 'antd';
 
 export const LayoutPageContainer = styled(Layout)`
-  height: 100%;
+  background-color: #ffffff;
 `;
