96ed01db8db79fe7da0bf7906e78cf0bd29ca323
jinsoo <admin@binary01.me>
2021-09-20 15:31:13 +0900

    feat : add Header component [UserInformation DropDown]

---
+++ src/components/layout/header/Menu/style.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import { Button as AntdButton } from 'antd';
+
+export const Button = styled(AntdButton)`
+  font-size: 16px;
+  font-weight: 600;
+`;
+
+export const LinkButton = styled(Button)`
+  color: rgba(0, 0, 0, 0.85);
+`;
