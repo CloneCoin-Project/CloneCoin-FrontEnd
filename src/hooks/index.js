31546ad7601606a331ec3174cdbbc662387144d7
dgujs <>
2021-09-20 15:54:02 +0900

    feat : Add custom hook for bithumb public Socket [refactoring needed]

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,1 +1,3 @@
 export { default as useScrollToTop } from '@/hooks/useScrollToTop';
+export { default as useTickerSocket } from '@/hooks/useTickerSocket';
+export { default as useTickerAllData } from '@/hooks/useTickerAllData';
