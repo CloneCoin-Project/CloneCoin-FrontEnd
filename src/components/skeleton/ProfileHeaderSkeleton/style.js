c47466f93863e6fe3b9d6641f6324ddb6d337e57
code4human <tena.kim10@gmail.com>
2021-09-22 09:14:02 +0900

    feat: Write skeleton for profile header

---
+++ src/components/skeleton/ProfileHeaderSkeleton/style.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import { Skeleton as AntdSkeleton, Card as AntdCard } from 'antd';
+
+export const Skeleton = styled(AntdSkeleton)`
+  margin-top: 16px;
+`;
+
+export const Card = styled(AntdCard)`
+  width: 30rem;
+  margin-bottom: 24px;
+  border: 1px solid #ffffff;
+`;
