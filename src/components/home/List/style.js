842504c9e961332b3312705e18ff6e4ed4a7c15d
jinsoo <admin@binary01.me>
2021-09-22 14:12:16 +0900

    feat : temporary list component add / typo modify

---
+++ src/components/home/List/style.js
@@ -0,0 +1,4 @@
+import { List, Avatar, Space } from 'antd';
+import { MessageOutlined, LikeOutlined, StarOutlined } from '@ant-design/icons';
+
+export { List, Avatar, Space, MessageOutlined, LikeOutlined, StarOutlined };
