a6cfc55602cdf3e66bcf3e7aad9e9953edb4e1d7
code4human <tena.kim10@gmail.com>
2021-10-02 03:43:02 +0900

    style: Apply neon style to header title

--- src/pages/layout/header/style.js
+++ src/pages/layout/header/style.js
@@ -1,16 +1,28 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { Link } from 'react-router-dom';
 import { Layout } from 'antd';
 
-import LogoPng from '@assets/images/logo.png';
+import BithumbGold from '@assets/images/bithumb_1.png';
 import { PROJECT_TITLE } from '@assets/string';
+
+const neon = keyframes`
+	0%,
+	100% {
+		text-shadow: 0 0 9px #EDC967, 0 0 9px #EDC967, 0 0 19px #EDC967, 0 0 3px #EDC967, 9px 1px 1px #FFFFFF;
+		color: #D2AC47;
+	}
+	50% {
+		text-shadow: 0 0 9px #E8D5A3, 0 0 9px #E8D5A3, 0 0 9px #E8D5A3, 0 0 3px #E8D5A3, 9px 1px 1px #FFFFFF;
+		color: #BD9A3F;
+	}
+`;
 
 const { Header } = Layout;
 
 export const LayoutPageHeader = styled(Header)`
   position: fixed;
   width: 100%;
-  height: 60px;
+  height: 80px;
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -31,10 +43,10 @@ export const LogoContainer = styled(Link)`
 `;
 
 export const LogoImage = styled.img.attrs({
-  src: LogoPng,
+  src: BithumbGold,
   alt: PROJECT_TITLE,
 })`
-  height: 32px;
+  height: 36px;
   max-width: 100%;
   max-height: 100%;
 `;
@@ -42,10 +54,12 @@ export const LogoImage = styled.img.attrs({
 export const LogoTitle = styled.span.attrs({
   title: PROJECT_TITLE,
 })`
-  font-size: 19px;
+  font-size: 24px;
   font-weight: 700;
-  color: #e48701;
-  white-space: nowrap;
+  font-style: italic;
+  color: #AE8625;
+  animation: ${ neon } infinite 4s linear;
+
   &::before {
     content: attr(title);
   }
