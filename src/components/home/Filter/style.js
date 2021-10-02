c5658d83e350acb33f08b4d70cebff4ac05742a8
code4human <tena.kim10@gmail.com>
2021-10-03 01:37:12 +0900

    style: Make RTQ table header sticky

--- src/components/home/Filter/style.js
+++ src/components/home/Filter/style.js
@@ -4,5 +4,8 @@ import { Divider as AntdDivider } from 'antd';
 export const Divider = styled(AntdDivider)`
   & .ant-divider-inner-text {
     font-size: 1.3rem;
+	font-weight: 700;
+	color: #D2AC47;
+	font-style: italic;
   }
 `;
--- src/components/home/RTQTable/style.js
+++ src/components/home/RTQTable/style.js
@@ -2,10 +2,15 @@ import styled from 'styled-components';
 import { Table as AntdTable } from 'antd';
 
 export const Table = styled(AntdTable)`
-	}
+    & thead tr th {
+        position: sticky;
+        top: 0;
+        z-index: 2;
+    }
 `;
 
 export const TableContainer = styled.div`
-	max-height: 500px;
-	overflow: auto;
+    position: relative;
+    max-height: 500px;
+    overflow: auto;
 `;
