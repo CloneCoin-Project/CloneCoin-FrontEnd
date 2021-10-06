2424660adefc9a222bb49d8dac4360db10711750
jinsoo <admin@binary01.me>
2021-10-06 11:03:26 +0900

    fix: unused RTQDrawer Delete

--- src/components/home/RTQTable/index.jsx
+++ src/components/home/RTQTable/index.jsx
@@ -2,31 +2,28 @@ import { useTickerSocket } from '@hooks';
 import { parseToDataSource, tickerColumns } from '@utils/bithumb';
 
 import * as S from '@components/home/RTQTable/style';
-import { Divider } from '@components/home/Filter/style';
-import { 
-	TABLE_TITLE, 
-	SCROLL_DOWN 
-} from '@assets/string';
+
+import { TABLE_TITLE, SCROLL_DOWN } from '@assets/string';
 
 const RTQTable = () => {
-	const { currentTickers } = useTickerSocket();
+  const { currentTickers } = useTickerSocket();
 
-	return (
-		<>
-			<S.DividerContainer>
-				<S.Ribbon text={ SCROLL_DOWN }>
-					<Divider orientation="left">{ TABLE_TITLE }</Divider>
-				</S.Ribbon>
-			</S.DividerContainer>
-			<S.TableContainer>
-				<S.Table
-					columns={tickerColumns}
-					dataSource={parseToDataSource(currentTickers)}
-					pagination={false}
-				/>
-			</S.TableContainer>
-		</>
-	);
+  return (
+    <>
+      <S.DividerContainer>
+        <S.Ribbon text={SCROLL_DOWN}>
+          <S.Divider orientation="left">{TABLE_TITLE}</S.Divider>
+        </S.Ribbon>
+      </S.DividerContainer>
+      <S.TableContainer>
+        <S.Table
+          columns={tickerColumns}
+          dataSource={parseToDataSource(currentTickers)}
+          pagination={false}
+        />
+      </S.TableContainer>
+    </>
+  );
 };
 
 export default RTQTable;
